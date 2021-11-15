c390d712a40878e458790f88573e6e698471455e
Napatji <napat12150@yahoo.com>
2021-11-16 01:25:20 +0700

    16/11/2021

--- Lab8/63010279_Lab8_3.py
+++ Lab8/63010279_Lab8_3.py
@@ -25,20 +25,60 @@ class BST:
                     self.insert(root.left, data)
                 elif self.checkChild(root.right) == False:
                     self.insert(root.right, data)
-                elif self.checkChild(root.left) == True and self.checkChild(root.right) == True:
-                    self.insert(root.left, data)
+                else:
+                    self.insert(self.findAlmostLeft(root), data)
         return root
+
+    def findAlmostLeft(self, node):
+        find = node
+        if not find.left.left:
+            return node
+        else:
+            return self.findAlmostLeft(find.left)
 
     def checkChild(self, node):
         if node.left and node.right:
             return True
         return False
 
-    def printTree(self, node, level = 0):
-        if node != None:
-            self.printTree(node.right, level + 1)
-            print('     ' * level, node)
-            self.printTree(node.left, level + 1)      
+    def subtract(self, root):
+        if not root:
+            return
+        left = root.left
+        right = root.right
+        if self.checkChild(left) == False and self.checkChild(right) == False:
+            temp_L = left.data
+            temp_R = right.data
+            root.data = int(min(temp_L,temp_R))
+            left.data = int(temp_L) - int(min(temp_L,temp_R))
+            right.data = int(temp_R) - int(min(temp_L,temp_R))
+        if self.checkChild(left) == True and self.checkChild(right) == True:
+            left = self.subtract(left)
+            right = self.subtract(right)
+            temp_L = left.data
+            temp_R = right.data
+            root.data = int(min(temp_L,temp_R))
+            left.data = int(temp_L) - int(min(temp_L,temp_R))
+            right.data = int(temp_R) - int(min(temp_L,temp_R))
+        if self.checkChild(left) == True and self.checkChild(right) == False:
+            left = self.subtract(left)
+            temp_L = left.data
+            temp_R = right.data
+            root.data = int(min(temp_L,temp_R))
+            left.data = int(temp_L) - int(min(temp_L,temp_R))
+            right.data = int(temp_R) - int(min(temp_L,temp_R))
+        if self.checkChild(left) == False and self.checkChild(right) == True:
+            right = self.subtract(right)
+            temp_L = left.data
+            temp_R = right.data
+            root.data = int(min(temp_L,temp_R))
+            left.data = int(temp_L) - int(min(temp_L,temp_R))
+            right.data = int(temp_R) - int(min(temp_L,temp_R))
+        return root
+
+def sumTree(node):
+    if  node != None:
+        pass
 
 def printTree(node, level = 0):
     if node != None:
@@ -51,13 +91,13 @@ if __name__ == "__main__":
     root = None
     node_number,node_value = input('Enter Input : ').split('/')
     node_value = node_value.split(' ')
-    # if (int(node_number) // 2) + 1 != len(node_value):
-    #     print('Incorrect Input')
+    if (int(node_number) // 2) + 1 != len(node_value):
+        print('Incorrect Input')
     if int(node_number) < 3:
         print('Incorrect Input')
     else:
         for i in range(1,int(node_number)+1):
-            root = tree.insert(root, i)
-        # for i in node_value:
-        #     root = tree.insert(root, int(i))
-    printTree(root)
+            if i < len(node_value):
+                root = tree.insert(root, 0)
+            else:
+                root = tree.insert(root, int(node_value[i-len(node_value)]))
--- Lab8/test.py
+++ Lab8/test.py
@@ -1,121 +1,38 @@
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-        self.height = 1
-
-    def __str__(self):
-        return str(self.val)
-
-class AVL_Tree:
-
-    def __init__(self):
-        self.root = None
-
-    def insert(self, root, key):
-		# Perform normal BST
-        if not root:
-            return TreeNode(key)
-        elif int(key) < int(root.val):
-            root.left = self.insert(root.left, key)
-        else:
-            root.right = self.insert(root.right, key)
-
-        root.height = 1 + max(self.getHeight(root.left),self.getHeight(root.right))
-
-        balance = self.getBalance(root)
-        left = root.left
-        right = root.right
-        if abs(balance) > 1:
-            if balance > 1 :
-                if int(key) == int(left.val):
-                    pass
-                if int(key) <= int(left.val):
-                    # Left Left
-                    return self.rightRotate(root)
-                if int(key) >= int(left.val):
-                    # Left Right
-                    left = self.leftRotate(left)
-                    return self.rightRotate(root)
-            if balance < -1 and int(key) >= int(right.val):
-                if int(key) >= int(right.val):
-                    # Right Right
-                    return self.leftRotate(root)
-                if int(key) <= int(right.val):
-                    # Right Left
-                    right = self.rightRotate(right)
-                    return self.leftRotate(root)
-        return root
-
-    def balanceTree(self, node):
-        if self.getHeight(node.left) != self.getHeight(node.right):
-            if self.getHeight(node.left) > self.getHeight(node.right):
-                check_node = node.left
-                balance = self.getBalance(check_node)
-                #left left
-                #left right
-                #right right
-                #right left
-
-            elif self.getHeight(root.left) < self.getHeight(node.right):
-                check_node = node.right
-                balance = self.getBalance(check_node)
-                #left left
-                #left right
-                #right right
-                #right left
-        return root
-                
-    def leftRotate(self, z):
-        y = z.right
-        x = y.left
-            # Perform rotation
-        y.left = z
-        z.right = x
-            # Update heights
-        z.height = 1 + max(self.getHeight(z.left),self.getHeight(z.right))
-        y.height = 1 + max(self.getHeight(y.left),self.getHeight(y.right))
-        return y
-
-    def rightRotate(self, z):
-        y = z.left
-        x = y.right
-            # Perform rotation
-        y.right = z
-        z.left = x
-            # Update heights
-        z.height = 1 + max(self.getHeight(z.left),self.getHeight(z.right))
-        y.height = 1 + max(self.getHeight(y.left),self.getHeight(y.right))
-            # Return the new root
-        return y
-
-    def getHeight(self, root):
-        if not root:
-            return 0
-        return root.height
-
-    def getBalance(self, root):
-        if not root:
-            return 0
-        return self.getHeight(root.left) - self.getHeight(root.right)         
-
-def printTree(node, level = 0):
-    if node != None:
-        printTree(node.right, level + 1)
-        print('     ' * level, node.val)
-        printTree(node.left, level + 1)
-
-if __name__ == "__main__":
-    T = AVL_Tree()
-    root = None
-    inp = input('Enter Input : ').split(' ')
-    for i in inp:
-        root = T.insert(root, i)
-        print(f'Insert : ( {i} )')
-        printTree(root)
-        print('--------------------------------------------------')
-    # print(T.getBalance(root))
-    # print(T.getHeight(root.left))
-    # print(T.getHeight(root.right))
-    # print(root.height)
+# if not root:
+#             return
+#         left = root.left
+#         right = root.right
+#         if self.checkChild(left) == False and self.checkChild(right) == False:
+#             temp_L = left.data
+#             temp_R = right.data
+#             root.data = int(min(temp_L,temp_R))
+#             left.data = int(temp_L) - int(min(temp_L,temp_R))
+#             right.data = int(temp_R) - int(min(temp_L,temp_R))
+#         if self.checkChild(left) == True and self.checkChild(right) == True:
+#             left = self.subtract(left)
+#             right = self.subtract(right)
+#             temp_L = left.data
+#             temp_R = right.data
+#             root.data = int(min(temp_L,temp_R))
+#             left.data = int(temp_L) - int(min(temp_L,temp_R))
+#             right.data = int(temp_R) - int(min(temp_L,temp_R))
+#         if self.checkChild(left) == True and self.checkChild(right) == False:
+#             left = self.subtract(left)
+#             temp_L = left.data
+#             temp_R = right.data
+#             root.data = int(min(temp_L,temp_R))
+#             left.data = int(temp_L) - int(min(temp_L,temp_R))
+#             right.data = int(temp_R) - int(min(temp_L,temp_R))
+#         if self.checkChild(left) == False and self.checkChild(right) == True:
+#             right = self.subtract(right)
+#             temp_L = left.data
+#             temp_R = right.data
+#             root.data = int(min(temp_L,temp_R))
+#             left.data = int(temp_L) - int(min(temp_L,temp_R))
+#             right.data = int(temp_R) - int(min(temp_L,temp_R))
+#         # else:
+#         #     if self.checkChild(left) == True:
+#         #         self.subtract(left)
+#         #     if self.checkChild(right) == True:
+#         #         self.subtract(right)
+#         return root
